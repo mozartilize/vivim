ef9e722a0d796fb9ba9d8a3842f1836dc9a5e12c
mozartilize <mozartilize@gmail.com>
2020-03-20 15:47:16 +0700

    graphql with gqlgen

--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module vivim
 go 1.12
 
 require (
+	github.com/99designs/gqlgen v0.11.3
 	github.com/cosmtrek/air v1.12.0 // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.15
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.0.1
 	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
 	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
--- server.go
+++ server.go
@@ -6,8 +6,16 @@ import (
 	"net/url"
 	"os/exec"
 
+	"context"
+
+	"vivim/graph"
+	"vivim/graph/generated"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 type person struct {
@@ -15,11 +23,60 @@ type person struct {
 	Age  int    `json:"age"`
 }
 
+func playgroundHandler() echo.HandlerFunc {
+	handler := playground.Handler("GraphQL Playground", "/graphql/query/")
+	return func(c echo.Context) error {
+		handler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
+
+func graphqlHandler() echo.HandlerFunc {
+	handler := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{Resolvers: &graph.Resolver{}}))
+	return func(c echo.Context) error {
+		handler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
+}
+
+type GqlgenContext struct {
+	echo.Context
+	ctx context.Context
+}
+
+func EchoContextToContextMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), "EchoContextKey", c)
+		c.SetRequest(c.Request().WithContext(ctx))
+
+		cc := &GqlgenContext{c, ctx}
+
+		return next(cc)
+	}
+}
+
 func main() {
 	config := readConfig("config", map[string]interface{}{})
 
 	e := echo.New()
 	e.Pre(middleware.AddTrailingSlash())
+
+	graphql := e.Group("/graphql")
+	// graphql.Use(EchoContextToContextMiddleware)
+	graphql.GET("/", playgroundHandler())
+	graphql.POST("/query/", graphqlHandler())
+
+	api := e.Group("/api/v1")
+	api.GET("/", func(c echo.Context) error {
+		u := &person{
+			Name: "world",
+			Age:  10000,
+		}
+		return c.JSON(http.StatusOK, u)
+	})
+
 	if staticDir := config.Get("STATIC_DIR"); staticDir == nil {
 		log.Printf("Starting webpack dev server...")
 		yarnServeCmd := exec.Command("yarn", "serve")
@@ -52,15 +109,6 @@ func main() {
 			}
 		})
 	}
-
-	api := e.Group("/api/v1")
-	api.GET("/", func(c echo.Context) error {
-		u := &person{
-			Name: "world",
-			Age:  10000,
-		}
-		return c.JSON(http.StatusOK, u)
-	})
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
